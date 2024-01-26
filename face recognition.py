294b1588aa0aab04a471971aa5f2b0d1d491be60
DeMi <125133861+AniketGaurav01@users.noreply.github.com>
2024-01-26 22:38:44 +0530

    Update face recognition.py

--- face recognition.py
+++ face recognition.py
@@ -4,93 +4,86 @@ import numpy as np
 import csv
 import os
 from datetime import datetime
- 
+
 video_capture = cv2.VideoCapture(0)
- 
-jobs_image = face_recognition.load_image_file("D:\com.whatsapp\WhatsApp\IMG_20200923_211531")
-jobs_encoding = face_recognition.face_encodings(aniket_image)[0]
- 
-ratan_tata_image = face_recognition.load_image_file("D:\com.whatsapp\WhatsApp\IMG-20200923-WA0001")
-ratan_tata_encoding = face_recognition.face_encodings(abhishek_image)[0]
- 
-sadmona_image = face_recognition.load_image_file("photos/xx.jpg")
-sadmona_encoding = face_recognition.face_encodings(xx_image)[0]
- 
-tesla_image = face_recognition.load_image_file("photos/elon.jpg")
-tesla_encoding = face_recognition.face_encodings(elon_image)[0]
- 
-known_face_encoding = [
-aniket_encoding,
-abhishek_encoding,
-xx_encoding,
-elon_encoding
-]
- 
-known_faces_names = [
-"aniket",
-"abhishek",
-"xx",
-"elon"
-]
- 
+
+aniket_image = face_recognition.load_image_file("C:/Users/91790/Downloads/aniket.webp")
+aniket_encoding = face_recognition.face_encodings(aniket_image)[0]
+
+abhishek_image = face_recognition.load_image_file("C:/Users/91790/Downloads/abhishek.webp")
+abhishek_encoding = face_recognition.face_encodings(abhishek_image)[0]
+
+xx_image = face_recognition.load_image_file("C:/Users/91790/Downloads/xx.webp")
+xx_encoding = face_recognition.face_encodings(xx_image)[0]
+
+elon_image = face_recognition.load_image_file("C:/Users/91790/Downloads/elon.webp")
+elon_encoding = face_recognition.face_encodings(elon_image)[0]
+
+known_face_encoding = [aniket_encoding, abhishek_encoding, xx_encoding, elon_encoding]
+
+known_faces_names = ["aniket", "abhishek", "xx", "elon"]
+
 students = known_faces_names.copy()
- 
+
 face_locations = []
 face_encodings = []
 face_names = []
-s=True
- 
- 
+s = True
+
 now = datetime.now()
 current_date = now.strftime("%Y-%m-%d")
- 
- 
- 
-f = open(current_date+'.csv','w+',newline = '')
+
+f = open(current_date + '.csv', 'w+', newline='')
 lnwriter = csv.writer(f)
- 
+
 while True:
-    _,frame = video_capture.read()
-    small_frame = cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
-    rgb_small_frame = small_frame[:,:,::-1]
+    _, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = small_frame[:, :, ::-1]
+
     if s:
         face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame,face_locations)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
         face_names = []
+
         for face_encoding in face_encodings:
-            matches = face_recognition.compare_faces(known_face_encoding,face_encoding)
-            name=""
-            face_distance = face_recognition.face_distance(known_face_encoding,face_encoding)
+            matches = face_recognition.compare_faces(known_face_encoding, face_encoding)
+            name = ""
+            face_distance = face_recognition.face_distance(known_face_encoding, face_encoding)
             best_match_index = np.argmin(face_distance)
+
             if matches[best_match_index]:
                 name = known_faces_names[best_match_index]
- 
+
             face_names.append(name)
+
             if name in known_faces_names:
                 font = cv2.FONT_HERSHEY_SIMPLEX
-                bottomLeftCornerOfText = (10,100)
-                fontScale              = 1.5
-                fontColor              = (255,0,0)
-                thickness              = 3
-                lineType               = 2
- 
-                cv2.putText(frame,name+' Present', 
-                    bottomLeftCornerOfText, 
-                    font, 
-                    fontScale,
-                    fontColor,
-                    thickness,
-                    lineType)
- 
+                bottomLeftCornerOfText = (10, 100)
+                fontScale = 1.5
+                fontColor = (255, 0, 0)
+                thickness = 3
+                lineType = 2
+
+                cv2.putText(frame, name + ' Present',
+                            bottomLeftCornerOfText,
+                            font,
+                            fontScale,
+                            fontColor,
+                            thickness,
+                            lineType)
+
                 if name in students:
                     students.remove(name)
                     print(students)
                     current_time = now.strftime("%H-%M-%S")
-                    lnwriter.writerow([name,current_time])
-    cv2.imshow("attendence system",frame)
+                    lnwriter.writerow([name, current_time])
+
+    cv2.imshow("attendance system", frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
- 
+
 video_capture.release()
 cv2.destroyAllWindows()
 f.close()
